4b3d0868119fd84d498e12cfd9ed4814f3f906a9
Tina Azimi <tazimiaraghi@scentregroup.com>
2024-08-10 16:32:54 +1000

    Add some initial pages

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,20 @@
+import Link from "next/link";
+
 const HomePage = () => {
-  return <div className="text-3xl">Home page</div>;
+  console.log("Hey there");
+  return (
+    <div>
+      <h1 className="text-2xl">Home Page</h1>
+      <Link
+        href={{
+          pathname: "/properties",
+          query: { filter: "all" },
+        }}
+      >
+        Properties
+      </Link>
+    </div>
+  );
 };
 
 export default HomePage;
