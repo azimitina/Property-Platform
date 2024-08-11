9cf37bf648afbdc659f98b1e82b82b8f81ab3e3c
Tina Azimi <tazimiaraghi@scentregroup.com>
2024-08-11 11:50:32 +1000

    Add properties - dummy data

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,14 @@
-import Link from "next/link";
+import Hero from "@/components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
+import HomeProperties from "@/components/HomeProperties";
 
 const HomePage = () => {
-  console.log("Hey there");
   return (
-    <div>
-      <h1 className="text-2xl">Home Page</h1>
-      <Link
-        href={{
-          pathname: "/properties",
-          query: { filter: "all" },
-        }}
-      >
-        Properties
-      </Link>
-    </div>
+    <>
+      <Hero />
+      <InfoBoxes />
+      <HomeProperties />
+    </>
   );
 };
 
--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,5 +1,16 @@
+import Properties from "@/components/Properties";
+
 const PropertiesPage = () => {
-  return <div>PropertiesPage</div>;
+  return (
+    <>
+      <section className="bg-blue-700 py-4">
+        <h1 className="text-white text-center text-2xl font-semibold">
+          Properties
+        </h1>
+      </section>
+      <Properties />
+    </>
+  );
 };
 
 export default PropertiesPage;
