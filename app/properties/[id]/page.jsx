a5206486cb156c1c8e068f81287924bd8cde31b1
Tina Azimi <tazimiaraghi@scentregroup.com>
2024-08-18 21:35:51 +1000

    Add PropertyImages

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -4,6 +4,7 @@ import Property from "@/models/Property";
 import Link from "next/link";
 import { FaArrowLeft } from "react-icons/fa";
 import PropertyDetails from "@/components/PropertyDetails";
+import PropertyImages from "@/components/PropertyImages";
 
 const PropertyPage = async ({ params }) => {
   await connectDB();
@@ -34,10 +35,10 @@ const PropertyPage = async ({ params }) => {
         <div className="container m-auto py-10 px-6">
           <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
             <PropertyDetails property={property} />
-            <aside className="space-y-4"></aside>
           </div>
         </div>
       </section>
+      <PropertyImages images={property.images} />
     </>
   );
 };
